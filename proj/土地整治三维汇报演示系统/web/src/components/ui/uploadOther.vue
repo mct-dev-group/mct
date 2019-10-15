2309dd81802291867c8d809cb5cbecb36c691351
z-zkang <z_zkang@163.com>
2019-10-15 17:36:42 +0800

    附件上传-文件类型判断

--- proj/土地整治三维汇报演示系统/web/src/components/ui/uploadOther.vue
+++ proj/土地整治三维汇报演示系统/web/src/components/ui/uploadOther.vue
@@ -10,6 +10,7 @@
     >
       <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
       <el-button style="margin-left: 10px;" size="small" type="success" @click="handleUpload">上传到服务器</el-button>
+      <div slot="tip" class="el-upload__tip">*只能上传'.txt','.doc','.pdf','.docx','.xls','.xlsx','.dppt'文件，且不超过50M</div>
     </el-upload>
   </div>
 </template>
@@ -21,7 +22,8 @@ export default {
   data () {
     return {
       fileList:new Map(),
-      DB:''
+      DB:'',
+      fileType:['txt','doc','pdf','docx','xls','xlsx','ppt']
     }
   },
   props:['gid'],
@@ -39,9 +41,14 @@ export default {
       if(this.fileList.size===0){
         this.$message.error('上传文件列表为空！');
         return;
-      }      
+      }
+      let arr=[...this.fileList.values()];      
+      let filterArr=arr.filter(v=>{
+        let type=v.name.split('.').slice(-1)[0];        
+        return this.fileType.includes(type);
+      });      
       const th=this;
-      const fds = [...this.fileList.values()].map(f => {
+      const fds = filterArr.map(f => {
         const fileInfo = f.name.split(".");
         const file_type = fileInfo.pop();
         const file_name = fileInfo.join(".");
@@ -52,22 +59,27 @@ export default {
         fd.append("DB", this.DB);
         fd.append("blob_data", f);
         return fd;
-      });      
+      });     
+      if(fds.length===0){
+        this.$message.error(`无合法文件！请检查后重新上传！`);
+        this.clearFiles();
+        return;
+      }else if(filterArr.length<arr.length){
+        this.$message.warning('检测到部分文件不合法，已过滤！');
+      }
       let promises=fds.map(fd =>post("/attachs/postAttachment",fd));
       Promise.all(promises).then(res=>{
-        if(res.every(r=>r.code===1)){
-          this.$message.success('上传成功!');
-        }else if(res.every(r=>r.code!==1)){
-          this.$message.error('上传失败!');
-        }else{
-          const errorArr=res.filter(r=>r.code!==1);
+        const errorArr=res.filter(r=>r.code!==1);
+        if(errorArr.length>0){
           let msg='';
           errorArr.forEach(v=>{
-           msg+=`${v.data.file_name}.${v.data.file_type} `
+            msg+=`${v.data.file_name}.${v.data.file_type} `
           });
           msg+='上传失败！';
           this.$message.error(msg);
-        }
+        }else{
+          this.$message.success('上传成功!');
+        }        
         this.clearFiles();
       }).catch(error=>{
         console.error('上传附件错误!',error);
